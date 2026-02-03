8bccf6cdcd261baf0c5f64b19984b800c04e46cf
Roger Florat Gutierrez <rflorat07@gmail.com>
2026-02-04 00:06:16 +0100

    Add CircularImage widget for displaying circular images with network and asset support

--- lib/core/widgets/widgets.dart
+++ lib/core/widgets/widgets.dart
@@ -1,3 +1,4 @@
 export '../router/scaffold_with_navbar.dart';
 export './containers/circular_icon.dart';
+export './containers/circular_image.dart';
 export './navigation/custom_bottom_nav_bar_item.dart';
